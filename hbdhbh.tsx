eb86b04e175bc71e133044724d87c2bc11f577d0
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-08-15 19:24:09 +0530

    Create hbdhbh.tsx

---
+++ hbdhbh.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HardcodedSecret: React.FC = () => {
+  const secret = 'supersecret'; // Hardcoded secret
+
+  return <div>Secret: {secret}</div>;
+};
+
+export default HardcodedSecret;
