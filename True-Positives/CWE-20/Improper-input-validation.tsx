bf7e0d6c0359355f3a7309c257dbde76abc95a8f
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-08-15 11:41:43 +0530

    Update Improper-input-validation.tsx

--- True-Positives/CWE-20/Improper-input-validation.tsx
+++ True-Positives/CWE-20/Improper-input-validation.tsx
@@ -1,35 +1,13 @@
-// src/components/UnsafeAuthComponent.tsx
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const UnsafeAuthComponent: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+interface Props {
+  username: string;
+}
 
-  const login = () => {
-    axios.post('/api/login', { username, password })
-      .then(response => {
-        localStorage.setItem('authToken', response.data.token); // Storing token insecurely
-      })
-      .catch(error => console.error('Login error:', error));
-  };
-
+const MyComponent: React.FC<Props> = ({ username }) => {
   return (
     <div>
-      <h1>Broken Authentication</h1>
-      <input
-        type="text"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <button onClick={login}>Login</button>
+      <p>Welcome, {username}</p>
     </div>
   );
 };
-
-export default UnsafeAuthComponent;
