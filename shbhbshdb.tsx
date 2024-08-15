3bb02df0dac2fde142aa32bba499dcc5419f03da
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-08-15 19:21:55 +0530

    Create shbhbshdb.tsx

---
+++ shbhbshdb.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HardcodedCredentials: React.FC = () => {
+  const username = 'admin';
+  const password = 'password123'; // Hardcoded credentials
+
+  return <div>Credentials are hardcoded!</div>;
+};
+
+export default HardcodedCredentials;
